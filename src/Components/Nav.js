961676583baa8ebd69b5a756ac9b953261794497
Younes Tahiri <you@example.com>
2022-01-18 20:21:32 -0500

    initial commit

---
+++ src/Components/Nav.js
@@ -0,0 +1,12 @@
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <div className="Nav">
+      <a href="/amount">Budget List</a>
+      <a href="/amount/new">Add New Budget</a>
+    </div>
+  );
+}
+
+export default Nav;
